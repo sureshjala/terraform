43c40c83d059406835296daf0848f59e29e75725
sureshjala <sureshjala7@gmail.com>
2022-11-10 12:31:50 +0530

    added new branch

--- backend.tf
+++ backend.tf
@@ -1,6 +1,7 @@
 terraform {
   backend "s3" {
-   key            = "LockID"
+    bucket         = "mybucketsuresh11"
+    key            = "LockID"
     region         = "us-west-2"
     dynamodb_table = "mydbsuresh"
   }
